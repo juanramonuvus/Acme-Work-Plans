8df5a03d3d8fe3cfb01be760b701054b887c9fc9
Alejandro Barranco Ledesma <alebarled@alum.us.es>
2021-04-18 12:59:34 +0200

    Task 007: supress some useless annotations

--- src/main/java/acme/validators/ValidDatesValidator.java
+++ src/main/java/acme/validators/ValidDatesValidator.java
@@ -2,12 +2,9 @@ package acme.validators;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-import javax.validation.constraintvalidation.SupportedValidationTarget;
-import javax.validation.constraintvalidation.ValidationTarget;
 
 import acme.entities.tasks.Task;
 
-@SupportedValidationTarget(ValidationTarget.PARAMETERS)
 public class ValidDatesValidator implements ConstraintValidator<ValidDates,Task>{
 	
 	@Override
