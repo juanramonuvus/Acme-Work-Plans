46a948e1dbaf80a3320b56c53c43156873391f1f
juanramonuvus <juaostrub@alum.us.es>
2021-05-16 17:53:03 +0200

    TASK 035: TERMINADA
    He realizado tanto pruebas positivas como negativas para la accion de
    registrarse como un usuario con rol "anonymous".

--- src/test/java/acme/testing/SignUpTest.java
+++ src/test/java/acme/testing/SignUpTest.java
@@ -12,9 +12,47 @@
 
 package acme.testing;
 
-public class SignUpTest {
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.openqa.selenium.By;
 
-	// This is a placeholder where you can introduce your own sign-up test
-  	// Note that it depends on your project-specific test class.
+public class SignUpTest extends AcmePlannerTest {
+
+	/*
+	 * In this test, a new anonymous user is created by going to the "Sing up" 
+	 * section and entering all the necessary data to register a user.
+	 */
 	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/sign-up/positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void positiveSingUp(final String username, final String password, final String name, final String surname, final String email){
+		super.signUp(username, password, name, surname, email, null);
+		super.signIn(username, password);
+		assert super.exists(By.linkText("Account"));
+		super.signOut();
+	}
+	
+	/*
+	 * In this test, a new anonymous user is created, accessing the "Sing up" section,
+	 *  and entering erroneous data, so that the system warns us that there are errors in the data entered.
+	 */
+	@ParameterizedTest
+	@CsvFileSource(resources = "/sign-up/negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(11)
+	public void negativeSingUp(final String username, final String password, final String name, final String surname, final String email) {
+		super.navigateHome();
+		super.clickOnMenu("Sign up", null);	
+		super.fillInputBoxIn("username", username);
+		super.fillInputBoxIn("password", password);
+		super.fillInputBoxIn("confirmation", password);
+		super.fillInputBoxIn("identity.name", name);
+		super.fillInputBoxIn("identity.surname", surname);
+		super.fillInputBoxIn("identity.email", email);
+		super.fillInputBoxIn("accept", "true");
+		super.clickOnSubmitButton("Sign up");
+		super.checkErrorsExist();
+		super.navigateHome();
+	}
 }
