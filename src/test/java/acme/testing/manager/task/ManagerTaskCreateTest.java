53cc9b887ff361c0c14002193777b9801f505ce9
Jesus Aparicio <72458998+JesusAparicioOrtiz@users.noreply.github.com>
2021-05-17 18:50:43 +0200

    Task-039: finished

--- src/test/java/acme/testing/manager/task/ManagerTaskCreateTest.java
+++ src/test/java/acme/testing/manager/task/ManagerTaskCreateTest.java
@@ -1,6 +1,7 @@
 package acme.testing.manager.task;
 
 import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.By;
@@ -12,14 +13,14 @@ public class ManagerTaskCreateTest extends AcmePlannerTest{
 	// Test cases -------------------------------------------------------------
 	
 	/* 
-	 * This test signs in as a manager, navigates into a personal task and updates its attributes.
+	 * This test signs in as a manager, navigates into create a task and create a task.
 	 * There shouldn't be any error, all data matches with restrictions.
 	 */
 	
 	@ParameterizedTest
 	@CsvFileSource(resources = "/manager/task/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
-	public void updatePositive(final int recordIndex, final String title,
+	public void createPositive(final int recordIndex, final String title,
 		final String executionStart, final String executionEnd,
 		final String workload, final String description, final String link,final String isPublic) {		
 		
@@ -33,9 +34,10 @@ public class ManagerTaskCreateTest extends AcmePlannerTest{
 		super.fillInputBoxIn("workload", workload);
 		super.fillInputBoxIn("description", description);
 		super.fillInputBoxIn("link", link);
-		final By inputLocator = By.id("isPublic$proxy");
-		super.clickAndGo(inputLocator);
-		
+		if(isPublic.equals("true")) {
+			final By inputLocator = By.id("isPublic$proxy");
+			super.locateOne(inputLocator).click();
+		}
 		super.clickOnSubmitButton("Create");
 		
 		super.clickOnMenu("Manager", "Personal Tasks");
@@ -56,41 +58,78 @@ public class ManagerTaskCreateTest extends AcmePlannerTest{
 	}
 	
 	/* 
-	 * This test signs in as a manager, navigates into a personal task and updates its attributes.
+	 * This test signs in as a manager, navigates into create a task and create a task.
 	 * There would rise some errors:
-	 * 	- Title: Text considerated spam.
-	 *  - Workload: The workload must be positive.
-	 *  - Link: Text considerated spam.
+	 * 	- Title: Must not be blank. Length must be between 1 and 80.
+	 *  - Execution start: May not be null.
+	 *  - Execution end: May not be null.
+	 *  - Workload: May not be null.
+	 *  - Description: Length must be between 1 and 500. Must not be blank.
 	 */
 	
-//	@ParameterizedTest
-//	@CsvFileSource(resources = "/manager/task/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
-//	@Order(20)
-//	public void updateNegative(final int recordIndex, final String title,
-//		final String executionStart, final String executionEnd,
-//		final String workload, final String description, final String link) {		
-//		
-//		super.signIn("manager1", "manag3r");
-//		
-//		super.clickOnMenu("Manager", "Personal Tasks");
-//		super.clickOnListingRecord(recordIndex);
-//
-//		super.fillInputBoxIn("title", title);
-//		super.fillInputBoxIn("executionStart", executionStart);
-//		super.fillInputBoxIn("executionEnd", executionEnd);
-//		super.fillInputBoxIn("workload", workload);
-//		super.fillInputBoxIn("description", description);
-//		super.fillInputBoxIn("link", link);		
-//		
-//		super.clickOnSubmitButton("Update");
-//		
-//		super.checkErrorsExist("title");
-//		super.checkErrorsExist("workload");
-//		super.checkErrorsExist("link");
-//		
-//		super.signOut();
-//		
-//		
-//	}
+	@Test
+	@Order(20)
+	public void createBlankNegative() {		
+		
+		super.signIn("manager1", "manag3r");
+		
+		super.clickOnMenu("Manager", "Create Task");
+		
+		super.clickOnSubmitButton("Create");
+		
+		super.checkErrorsExist("title");
+		super.checkErrorsExist("executionStart");
+		super.checkErrorsExist("executionEnd");
+		super.checkErrorsExist("workload");
+		
+		String xpath;
+		By locator;
+		xpath = String.format("//div[@class='form-group'][textarea[@id='%s'] and div[@class='text-danger']]", "description");
+		locator = By.xpath(xpath);
+		assert super.exists(locator) : String.format("No errors found in input box '%s'", "description");
+		
+		super.signOut();
+		
+	}
+	
+	/* 
+	 * This test signs in as a manager, navigates into create a task and create a task.
+	 * There would rise some errors:
+	 * 	- Title: Must not be blank. Length must be between 1 and 80.
+	 *  - Execution start: Must be in the future. 
+	 *  - Execution end: Must be in the future. 
+	 *  - Workload: The workload value must be positive. 
+	 *  - link: Not a well-formed URL.
+	 */
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/manager/task/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(30)
+	public void updateNegative(final int recordIndex, final String title,
+		final String executionStart, final String executionEnd,
+		final String workload, final String description, final String link,final String isPublic) {		
+		
+		super.signIn("manager1", "manag3r");
+		
+		super.clickOnMenu("Manager", "Create Task");
+
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("executionStart", executionStart);
+		super.fillInputBoxIn("executionEnd", executionEnd);
+		super.fillInputBoxIn("workload", workload);
+		super.fillInputBoxIn("description", description);
+		super.fillInputBoxIn("link", link);		
+		
+		super.clickOnSubmitButton("Create");
+		
+		super.checkErrorsExist("executionStart");
+		super.checkErrorsExist("executionEnd");
+		super.checkErrorsExist("workload");
+		super.checkErrorsExist("link");
+		
+		super.signOut();
+		
+		
+	}
 
 }
