ab4104128abeb267db5a9a1506d9fd552d5be9e7
Alejandro Barranco Ledesma <alebarled@alum.us.es>
2021-05-19 23:54:56 +0200

    Task-043: all constraints tested

--- src/test/java/acme/testing/manager/task/ManagerTaskUpdateTest.java
+++ src/test/java/acme/testing/manager/task/ManagerTaskUpdateTest.java
@@ -3,6 +3,7 @@ package acme.testing.manager.task;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.openqa.selenium.By;
 
 import acme.testing.AcmePlannerTest;
 
@@ -59,10 +60,22 @@ public class ManagerTaskUpdateTest extends AcmePlannerTest{
 	
 	/* 
 	 * This test signs in as a manager, navigates into a personal task and updates its attributes.
-	 * There would rise some errors:
-	 * 	- Title: Text considerated spam.
-	 *  - Workload: The workload must be positive.
-	 *  - Link: Text considerated spam.
+	 * Every row in csv file is a different case with different errors:
+	 * 
+	 * 	- case 0: All fields blank, errors would rise in every inputbox except link (optional).
+	 * 
+	 *  - case 1: Title, description and link considerated spam. Execution start, execution end
+	 *  and workload are invalid values.
+	 *  
+	 *  - case 2: Error would rise in execution end because start date must be earlier than end date.
+	 *  An error would rise in workload because its value is negative. An error would rise in link because
+	 *  is not a well-formed URL.
+	 *  
+	 *  - case 3: Error would rise in workload because it must fix in dates period.
+	 *  
+	 *  - case 4: Error would rise in workload because value must contain 2 decimal figures or less.
+	 *  
+	 *  - case 5: Error would rise in workload because value must be between 0 and 59 (minutes).
 	 */
 	
 	@ParameterizedTest
@@ -70,12 +83,12 @@ public class ManagerTaskUpdateTest extends AcmePlannerTest{
 	@Order(20)
 	public void updateNegative(final int recordIndex, final String title,
 		final String executionStart, final String executionEnd,
-		final String workload, final String description, final String link) {		
+		final String workload, final String description, final String link, final String isPublic) {		
 		
 		super.signIn("manager1", "manag3r");
 		
 		super.clickOnMenu("Manager", "Personal Tasks");
-		super.clickOnListingRecord(recordIndex);
+		super.clickOnListingRecord(0);
 
 		super.fillInputBoxIn("title", title);
 		super.fillInputBoxIn("executionStart", executionStart);
@@ -83,15 +96,58 @@ public class ManagerTaskUpdateTest extends AcmePlannerTest{
 		super.fillInputBoxIn("workload", workload);
 		super.fillInputBoxIn("description", description);
 		super.fillInputBoxIn("link", link);		
+		if(isPublic.equals("true")) {
+			final By inputLocator = By.id("isPublic$proxy");
+			super.locateOne(inputLocator).click();
+		}
 		
 		super.clickOnSubmitButton("Update");
 		
-		super.checkErrorsExist("title");
-		super.checkErrorsExist("workload");
-		super.checkErrorsExist("link");
+		String xpath;
+		By locator;
+		xpath = String.format("//div[@class='form-group'][textarea[@id='%s'] and div[@class='text-danger']]", "description");
+		locator = By.xpath(xpath);
 		
-		super.signOut();
+		//check different errors for each row in csv file
 		
+		switch(recordIndex) {
+		
+		case 0:
+			super.checkErrorsExist("title");
+			super.checkErrorsExist("executionStart");
+			super.checkErrorsExist("executionEnd");
+			super.checkErrorsExist("workload");
+			assert super.exists(locator) : String.format("No errors found in input box '%s'", "description");
+			break;
+		
+		case 1:
+			super.checkErrorsExist("title");
+			super.checkErrorsExist("executionStart");
+			super.checkErrorsExist("executionEnd");
+			super.checkErrorsExist("workload");
+			super.checkErrorsExist("link");
+			assert super.exists(locator) : String.format("No errors found in input box '%s'", "description");
+			break;
+		
+		case 2:
+			super.checkErrorsExist("executionEnd");
+			super.checkErrorsExist("workload");
+			super.checkErrorsExist("link");
+			break;
+		
+		case 3:
+			super.checkErrorsExist("workload");
+			break;
+		
+		case 4:
+			super.checkErrorsExist("workload");
+			break;
+			
+		case 5:
+			super.checkErrorsExist("workload");
+			break;
+			
+		}
 		
 	}
 
