39a00f8be5acc50cb17731f788be533644358f3f
davbrican <72500941+davbrican@users.noreply.github.com>
2021-05-20 13:18:20 +0200

    Task-045: Done to review

---
+++ src/test/java/acme/testing/administrator/spamPanel/AdministratorThresholdList.java
@@ -0,0 +1,39 @@
+package acme.testing.administrator.spamPanel;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.AcmePlannerTest;
+
+public class AdministratorThresholdList extends AcmePlannerTest{
+	
+	// Test cases -------------------------------------------------------------
+	/* 
+	 * As an administrator, test that can see the spam panel configuration and the asociated threshold
+	 * There shouldn't be any error.
+	*/
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/administrator/spamPanel/threshold.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void listPositive(final int index, final String threshold) {		
+		
+		super.signIn("administrator", "administrator");
+		super.clickOnMenu("Administrator", "Spam settings");
+		
+		super.checkColumnHasValue(index, 0, threshold);
+	}
+	
+	@Test
+	@Order(20)
+	public void listNegative() {
+		super.signIn("manager1", "manag3r");
+		
+		super.navigate("/administrator/threshold/list", "");
+		super.checkErrorsExist();
+		
+	}
+	
+}
